8bd83bc547b316ff907a437ce6c3bcdeae4e242f
EvidenceKiller <zxn18789469611@gmail.com>
2015-07-14 17:20:34 +0800

    completed

---
+++ 05_intent/src/main/java/com/zxn/intent/BasicViewActivity.java
@@ -0,0 +1,17 @@
+package com.zxn.intent;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 2015/7/14.
+ */
+public class BasicViewActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.basic_view);
+    }
+
+}
--- 05_intent/src/main/java/com/zxn/intent/MainActivity.java
+++ 05_intent/src/main/java/com/zxn/intent/MainActivity.java
@@ -2,37 +2,52 @@ package com.zxn.intent;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    private final static String tag = "MainActivity";
+
+    // Initialize this in onCreateOptions
+    Menu myMenu = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.setupButton();
+        this.setupEditText();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
+        super.onCreateOptionsMenu(menu);
+        this.myMenu = menu;
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        try {
+            handleMenus(item);
+        } catch (Throwable t) {
+            Log.d(tag, t.getMessage(), t);
+            throw new RuntimeException("error", t);
         }
+        return true;
+    }
 
-        return super.onOptionsItemSelected(item);
+    private void handleMenus(MenuItem item) {
+        this.appendMenuItemText(item);
+        if(item.getItemId() == R.id.menu_clear) {
+            this.emptyText();
+        } else if(item.getItemId() == R.id.menu_basic_view) {
+            IntentsUtils.invokeBasicActivity(this);
+        }
     }
 }
