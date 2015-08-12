42114fddd9299bc2aaf8d20b781e048b68da9395
EvidenceKiller <zxn18789469611@gmail.com>
2015-08-12 18:00:24 +0800

    ButtonActivity

--- 06_controls/src/main/java/com/zxn/controls/MainActivity.java
+++ 06_controls/src/main/java/com/zxn/controls/MainActivity.java
@@ -1,38 +1,42 @@
 package com.zxn.controls;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.ListActivity;
+import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ListActivity implements AdapterView.OnItemClickListener {
+
+    private String[] activities = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        Resources res = getResources();
+
+        activities = res.getStringArray(R.array.list_activities);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.list_titles, android.R.layout.simple_list_item_1);
+
+        this.setListAdapter(adapter);
+        ListView listView = getListView();
+        listView.setOnItemClickListener(this);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        try {
+            Intent intent = new Intent(this, Class.forName("com.zxn.controls." + activities[i]));
+            startActivity(intent);
+        } catch (Exception e) {
+            Log.e("CommonControls", "Problem with activity list for main menu");
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
